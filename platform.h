f821db82e867bf729d8449348c52d82fc1a8e11d
LuanFabricio <luanlimaleite@hotmail.com>
2024-03-02 10:54:16 -0300

    feat: Adding a platform header, for specific platform implementation

---
+++ platform.h
@@ -0,0 +1,23 @@
+#ifndef PLATFORM_H_
+#define PLATFORM_H_
+
+#include "types.h"
+
+#include "raylib.h"
+
+extern void Platform_init_window(u32 w, u32 h, const char* title);
+extern void Platform_close_Window();
+extern boolean Platform_window_should_close();
+
+extern void Platform_begin_draw();
+extern void Platform_end_draw();
+
+extern void Platform_clear_background(u32 color);
+extern void Platform_draw_point(float x, float y, float radius, u32 color);
+
+extern float Platform_rand_float();
+extern float Platform_rand_range(float min, float max);
+
+extern void Platform_log_mem(float mem_usage);
+
+#endif // PLATFORM_H_
